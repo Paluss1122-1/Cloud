d98dfb2020da208f65f471dfc6ddb08bc0194b93
Paluss1122-1 <Paluss1122@gmail.com>
2025-10-22 16:52:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cloud/Config.kt
@@ -0,0 +1,7 @@
+package com.example.cloud
+
+object SupabaseConfig {
+    const val SUPABASE_URL = "https://oulgglfvobyjmfongnil.supabase.co"
+    const val SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91bGdnbGZ2b2J5am1mb25nbmlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2MzQ5NzgsImV4cCI6MjA3NjIxMDk3OH0.5Y-jhzuIJZ0xOQ4WPn9mjnCo2dcxO0AOx1Hkaur5Sc4"
+    const val SUPABASE_BUCKET = "Files"
+}
