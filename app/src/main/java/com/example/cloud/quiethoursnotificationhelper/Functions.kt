df0842dd81ab072b357319d32123a55a0b7973f3
Paluss1122-1 <Paluss1122@gmail.com>
2026-01-05 11:22:50 +0100

    functions for podcasts out of 'QuietHoursNotificationService' moved to own files ('Functions-kt', 'Podcast.kt')

---
+++ app/src/main/java/com/example/cloud/quiethoursnotificationhelper/Functions.kt
@@ -0,0 +1,44 @@
+package com.example.cloud.quiethoursnotificationhelper
+
+import android.app.NotificationManager
+import android.content.Context
+import com.example.cloud.ERRORINSERT
+import com.example.cloud.ERRORINSERTDATA
+import com.example.cloud.showSimpleNotificationExtern
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.time.Instant
+import kotlin.time.Duration
+
+
+fun markMessageAsRead(messageId: String, readMessageIds:  MutableSet<String>, context: Context) {
+    try {
+        readMessageIds.add(messageId)
+
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val parts = messageId.split("_")
+        if (parts.size >= 2) {
+            val sender = parts[0]
+
+            val senderHash = sender.hashCode()
+            for (i in 0..10) {
+                val partNotificationId = senderHash + 10000 + i
+                notificationManager.cancel(partNotificationId)
+            }
+        }
+
+        showSimpleNotificationExtern("✅ Markiert","Nachricht wurde als gelesen markiert", Duration.ZERO, context)
+    } catch (e: Exception) {
+        CoroutineScope(Dispatchers.IO).launch {
+            ERRORINSERT(
+                ERRORINSERTDATA(
+                    "markMessageAsRead",
+                    "Fehler bei Nachricht als gelesen markieren: ${e.message}",
+                    Instant.now().toString(),
+                    "Error"
+                )
+            )
+        }
+    }
+}
