1125180b36c3ff3826e8c0b43aae6dc40f969aa3
Paluss1122-1 <Paluss1122@gmail.com>
2026-02-14 12:39:39 +0100

    added

---
+++ app/src/main/java/com/example/cloud/privatecloudapp/getVideoFirstFrame.kt
@@ -0,0 +1,17 @@
+package com.example.cloud.privatecloudapp
+
+import android.graphics.Bitmap
+import android.media.MediaMetadataRetriever
+import java.io.File
+
+fun getVideoFirstFrame(file: File): Bitmap? {
+    return try {
+        val retriever = MediaMetadataRetriever()
+        retriever.setDataSource(file.absolutePath)
+        val bitmap = retriever.getFrameAtTime(0, MediaMetadataRetriever.OPTION_CLOSEST_SYNC)
+        retriever.release()
+        bitmap
+    } catch (_: Exception) {
+        null
+    }
+}
