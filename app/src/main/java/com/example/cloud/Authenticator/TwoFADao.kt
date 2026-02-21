a8f09f3bd673cb207b06cdea9b3acd37e3c35e78
Paluss1122-1 <Paluss1122@gmail.com>
2026-02-21 14:36:40 +0100

    removed folders

--- app/src/main/java/com/example/cloud/Authenticator/TwoFADao.kt
+++ app/src/main/java/com/example/cloud/Authenticator/TwoFADao.kt
@@ -4,8 +4,6 @@ import androidx.room.*
 
 @Dao
 interface TwoFADao {
-    @Query("SELECT * FROM twofa_entries WHERE folder = :folder")
-    suspend fun getByFolder(folder: String?): List<TwoFAEntry>
     @Query("SELECT * FROM twofa_entries")
     suspend fun getAll(): List<TwoFAEntry>
 
