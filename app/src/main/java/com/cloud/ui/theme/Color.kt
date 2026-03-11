3bdc9288fc65002b9e89e7f80663724cd8d53353
Paluss1122-1 <Paluss1122@gmail.com>
2026-03-11 01:29:44 +0100

    updated packagename

--- app/src/main/java/com/cloud/ui/theme/Color.kt
+++ app/src/main/java/com/cloud/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.cloud.ui.theme
+package com.cloud.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/cloud/ui/theme/Theme.kt
+++ app/src/main/java/com/cloud/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.cloud.ui.theme
+package com.cloud.ui.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
