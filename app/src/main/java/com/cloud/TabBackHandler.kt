520c3147c9c740096a55c028c75788bf6a85bb67
Paluss1122-1 <paluss1122@gmail.com>
2026-04-13 20:02:34 +0200

    removed comments + fixed wrong file placement

--- app/src/main/java/com/cloud/TabBackHandler.kt
+++ app/src/main/java/com/cloud/TabBackHandler.kt
@@ -1,16 +1,16 @@
+package com.cloud
+
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-// TabNavigationState.kt
 data class TabNavigationState(
     val canNavigateBack: Boolean = false,
     val onNavigateBack: () -> Unit = {}
 )
 
-// TabNavigationViewModel.kt
 class TabNavigationViewModel : ViewModel() {
 
     private val _navigationState = MutableStateFlow(TabNavigationState())
