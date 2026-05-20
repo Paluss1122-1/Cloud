2a7f82704fae6be7499c6a7b2f4c90de910e637e
Paluss1122-1 <Paluss1122@gmail.com>
2026-05-21 01:32:07 +0200

    added weights for every model (for usage limits)

--- app/src/main/java/com/cloud/tabs/aitab/Models.kt
+++ app/src/main/java/com/cloud/tabs/aitab/Models.kt
@@ -3,19 +3,29 @@ package com.cloud.tabs.aitab
 data class Model(
     val realname: String,
     val vision: Boolean = false,
+    val weight: Int = 1,
     val name: String = realname.substringAfter("/", realname)
 )
 
 val nvidiaModels = listOf(
-    Model("meta/llama-3.1-8b-instruct"),
-    Model("openai/gpt-oss-120b"),
-    Model("openai/gpt-oss-20b"),
-    Model("nvidia/nemotron-3-nano-30b-a3b"),
-    Model("nvidia/nemoretriever-ocr-v1", true),
-    Model("meta/llama-3.2-90b-vision-instruct", true),
-    Model("meta/llama-4-maverick-17b-128e-instruct", true),
-    Model("qwen/qwen3.5-397b-a17b", true),
+    Model("meta/llama-3.1-8b-instruct", weight = 1),
+    Model("openai/gpt-oss-120b", weight = 2),
+    Model("openai/gpt-oss-20b", weight = 1),
+    Model("nvidia/nemotron-3-nano-30b-a3b", weight = 2),
+    Model("nvidia/nemoretriever-ocr-v1", vision = true, weight = 1),
+    Model("meta/llama-3.2-90b-vision-instruct", vision = true, weight = 2),
+    Model("meta/llama-4-maverick-17b-128e-instruct", vision = true, weight = 2),
+    Model("qwen/qwen3.5-397b-a17b", vision = true, weight = 4)
 )
+
+val geminiModels = listOf(
+    Model("gemini-3.1-flash-lite", vision = true, weight = 2),
+    Model("gemini-3-flash-preview", vision = true, weight = 2),
+    Model("gemini-2.5-pro", vision = true, weight = 3),
+    Model("gemini-2.5-flash", vision = true, weight = 2),
+    Model("gemini-2.5-flash-lite", vision = true, weight = 1)
+)
+
 val serverModels = listOf(
     Model("qwen2.5:7b"),
     Model("qwen2.5-coder:3b"),
