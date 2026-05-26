501672ae9871f79c6eb3a40bdd2c80468af5b3fb
Paluss1122-1 <paluss1122@gmail.com>
2026-05-26 08:59:30 +0200

    removed Old remember for variables

--- app/src/main/java/com/cloud/tabs/aitab/ViewModel.kt
+++ app/src/main/java/com/cloud/tabs/aitab/ViewModel.kt
@@ -54,9 +54,8 @@ class AITabViewModel(application: Application) : AndroidViewModel(application) {
     var editIndex: Int? by mutableStateOf(null)
     var isEditMode by mutableStateOf(false)
     var currentEditMsg by mutableStateOf("")
-    var selectedMsg: Int? by remember { mutableStateOf(null) }
-    var lastSelectedMsg: Int? by remember { mutableStateOf(null) }
-
+    var selectedMsg: Int? by mutableStateOf(null)
+    var lastSelectedMsg: Int? by mutableStateOf(null)
     val history = mutableStateListOf<ChatMessage>()
 
     val availableModels
