5dfe99af31c996b410b3a4b1ba875b32473225ed
Paluss1122-1 <Paluss1122@gmail.com>
2026-06-04 10:37:19 +0200

    renamed app

--- wrapper.py
+++ wrapper.py
@@ -4,7 +4,7 @@ import sys
 import time
 
 while True:
-    result = subprocess.run([sys.executable, r"C:\Users\Paul\AndroidStudioProjects\Cloud\postcommit.py"])
+    result = subprocess.run([sys.executable, r"C:\Users\Paul\AndroidStudioProjects\Tabslify\postcommit.py"])
     if result.returncode == 0:
         break
     print(f"[wrapper] crashed (code {result.returncode}), restarting in 5s...")
