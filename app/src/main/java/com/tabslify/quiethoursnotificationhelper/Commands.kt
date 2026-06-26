d2349a100306a33f68ebc4692feb76c55b2eb0ed
Paluss1122-1 <Paluss1122@gmail.com>
2026-06-26 13:12:19 +0200

    remove no longer needed commands ('import' / 'export')

--- app/src/main/java/com/tabslify/quiethoursnotificationhelper/Commands.kt
+++ app/src/main/java/com/tabslify/quiethoursnotificationhelper/Commands.kt
@@ -167,43 +167,6 @@ fun getHomeWifiStatus(
 
 private fun getAvailableCommands(context: Context): List<Command> {
     return listOf(
-        /*Command(
-            name = "import",
-            aliases = listOf("migration"),
-            description = "Startet Launcher zum JSON Import"
-        ) {
-            val intent = Intent(context, ImportActivity::class.java).apply {
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            }
-            context.startActivity(intent)
-        },
-        Command(
-            name = "export",
-            aliases = listOf("migration"),
-            description = "Exportiert die Einstellungen in eine JSON-Datei"
-        ) {
-            try {
-                val uri = MigrationManager.exportPrefs(context)
-                val intent = Intent(Intent.ACTION_SEND).apply {
-                    type = "application/json"
-                    putExtra(Intent.EXTRA_STREAM, uri)
-                    addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                }
-                val chooser = Intent.createChooser(intent, "Einstellungen exportieren").apply {
-                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                }
-                context.startActivity(chooser)
-                Toast.makeText(context, "Export erfolgreich", Toast.LENGTH_SHORT).show()
-            } catch (e: Exception) {
-                showSimpleNotificationExtern(
-                    "❌ Fehler beim Export",
-                    e.message ?: "Unbekannter Fehler",
-                    context = context,
-                    silent = false
-                )
-            }
-        },*/
         Command(
             name = "r",
             aliases = listOf("migration"),
