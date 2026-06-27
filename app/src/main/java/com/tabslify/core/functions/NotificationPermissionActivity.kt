033c526c2b11cd7f2339fa774f0fb37ffa3a70bc
Paluss1122-1 <Paluss1122@gmail.com>
2026-06-27 06:54:11 +0200

    add NotificationPermissionActivity to launch instant permission launcher for post_notifications

---
+++ app/src/main/java/com/tabslify/core/functions/NotificationPermissionActivity.kt
@@ -0,0 +1,27 @@
+package com.tabslify.core.functions
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+
+class NotificationPermissionActivity : ComponentActivity() {
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { _ ->
+        finish()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            finish()
+            return
+        }
+
+        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+    }
+}
