8ece4a00a3d7c66ef46100289125e655da04d1bc
Paluss1122-1 <Paluss1122@gmail.com>
2026-07-01 23:53:00 +0200

    undo canNotify expand

--- app/src/main/java/com/tabslify/core/functions/PermissionChecker.kt
+++ app/src/main/java/com/tabslify/core/functions/PermissionChecker.kt
@@ -2,19 +2,8 @@ package com.tabslify.core.functions
 
 import android.Manifest
 import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.widget.Toast
-import com.tabslify.core.objects.toast
 
 fun canNotify(context: Context): Boolean {
-    val granted =
-        context.checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
-    if (!granted) {
-        val intent = Intent(context, NotificationPermissionActivity::class.java).apply {
-            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        }
-        context.startActivity(intent)
-    }
-    return granted
+    return context.checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
 }
