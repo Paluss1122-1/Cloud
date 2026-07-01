5cc4c82a49a5783d2728a819c3c09d650f8d5563
Paluss1122-1 <Paluss1122@gmail.com>
2026-07-02 01:04:55 +0200

    add restrict logic for QuietHoursNotificationService

--- app/src/main/java/com/tabslify/core/BootReceiver.kt
+++ app/src/main/java/com/tabslify/core/BootReceiver.kt
@@ -2,12 +2,18 @@ package com.tabslify.core
 
 import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import android.content.Intent
 import androidx.core.content.ContextCompat
 import com.tabslify.services.QuietHoursNotificationService
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
+        val prefs = context.getSharedPreferences("app_prefs", MODE_PRIVATE)
+        if (!prefs.getBoolean("services_master", false) || !prefs.getBoolean("service_qhns", false)) {
+            return
+        }
+
         val serviceIntent = Intent(context, QuietHoursNotificationService::class.java)
         try {
             ContextCompat.startForegroundService(context, serviceIntent)
