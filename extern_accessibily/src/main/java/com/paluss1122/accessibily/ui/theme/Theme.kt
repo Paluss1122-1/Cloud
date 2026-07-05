8e7f4ea351ba86e8f1e5da3fa257c94276287273
Paluss1122-1 <Paluss1122@gmail.com>
2026-07-05 23:19:06 +0200

    remove unused import

--- extern_accessibily/src/main/java/com/paluss1122/accessibily/ui/theme/Theme.kt
+++ extern_accessibily/src/main/java/com/paluss1122/accessibily/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.paluss1122.accessibily.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
