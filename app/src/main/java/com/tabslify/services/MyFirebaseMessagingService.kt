e48822e21f05703964bd42064ec9af93c23306e9
Paluss1122-1 <Paluss1122@gmail.com>
2026-07-24 23:49:32 +0200

    show push notification for new email fcm messages

--- app/src/main/java/com/tabslify/services/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/tabslify/services/MyFirebaseMessagingService.kt
@@ -5,6 +5,9 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.tabslify.core.activities.Tabslify
 import com.tabslify.core.activities.fetchAndRun
+import com.tabslify.core.functions.showSimpleNotificationExtern
+import com.tabslify.core.objects.prvt
+import com.tabslify.core.objects.toast
 import kotlinx.coroutines.launch
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
@@ -12,10 +15,28 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         super.onNewToken(token)
         FirebaseMessaging.getInstance()
             .subscribeToTopic("all_users")
+        if (prvt()) {
+            FirebaseMessaging.getInstance()
+                .subscribeToTopic("emails")
+        }
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
+        println("Message received: $remoteMessage")
+        if (remoteMessage.from?.endsWith("emails") == true && prvt()) {
+            println("Email notification received")
+            val title = remoteMessage.notification?.title ?: remoteMessage.data["title"] ?: return
+            val body = remoteMessage.notification?.body ?: remoteMessage.data["body"] ?: ""
+            println("Title: $title, Body: $body")
+            showSimpleNotificationExtern(
+                title = title,
+                text = body,
+                context = applicationContext,
+                silent = false
+            )
+            return
+        }
         val scriptName = remoteMessage.data["script_name"] ?: return
         Tabslify.serviceScope.launch {
             try {
