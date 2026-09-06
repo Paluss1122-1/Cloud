0a1057645ecee63eae7708819768f0a3683a3871
Paluss1122-1 <Paluss1122@gmail.com>
2026-09-06 11:45:09 +0200

    supress depraction

--- app/src/main/java/com/tabslify/services/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/tabslify/services/MyFirebaseMessagingService.kt
@@ -14,6 +14,7 @@ import com.tabslify.core.objects.tNotify
 import kotlinx.coroutines.launch
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
+    @Suppress("DEPRECATION", "OVERRIDE_DEPRECATION")
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         FirebaseMessaging.getInstance()
